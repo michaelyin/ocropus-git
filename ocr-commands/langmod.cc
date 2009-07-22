f0c952b0c77092e03e7bd0ccfbbe01b53c640da0
tmb <tmb@ixion>
2009-07-22 08:43:05 +0200

    handled fst not found in fsts2text

--- ocr-commands/langmod.cc
+++ ocr-commands/langmod.cc
@@ -1,20 +1,3 @@
-// -*- C++ -*-
-
-// Copyright 2006-2007 Deutsches Forschungszentrum fuer Kuenstliche Intelligenz
-// or its licensors, as applicable.
-//
-// You may not use this file except under the terms of the accompanying license.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you
-// may not use this file except in compliance with the License. You may
-// obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
 // Project:
 // File:
 // Purpose:
@@ -168,7 +151,13 @@ namespace ocropus {
                     int line = bookstore->getLineId(page,j);
                     debugf("progress","page %04d %06x\n",page,line);
                     autodel<OcroFST> fst(make_OcroFST());
-                    fst->load(bookstore->path(page,line,0,"fst"));
+                    try {
+                        fst->load(bookstore->path(page,line,0,"fst"));
+                    } catch(const char *error) {
+                        fprintf(stderr,"%04d %06x: can't load fst: %s\n",page,line,error);
+                        // if(abort_on_error) abort();
+                        continue;
+                    }
                     nustring str;
                     try {
                         intarray v1;
