e0aed5dd69234dd3ad52316acf709d9a868bd5ad
tmb <tmb@haumea>
2009-07-31 03:42:17 +0200

    added special purpose I/O methods for segmentations etc. in IBookStore

--- ocr-utils/bookstore.h
+++ ocr-utils/bookstore.h
@@ -26,6 +26,71 @@ namespace ocropus {
         virtual int numberOfPages() = 0;
         virtual int linesOnPage(int i) = 0;
         virtual int getLineId(int i,int j) = 0;
+
+        void getLineBin(bytearray &image,int page,int line,const char *variant=0) {
+            strg v = "bin";
+            if(variant) { v += "."; v += variant; }
+            getLine(image,page,line,variant);
+        }
+        void putLineBin(bytearray &image,int page,int line,const char *variant=0) {
+            strg v = "bin";
+            if(variant) { v += "."; v += variant; }
+            putLine(image,page,line,variant);
+        }
+
+        void getPageSegmentation(intarray &image,int page,const char *variant=0) {
+            strg v = "pseg";
+            if(variant) { v += "."; v += variant; }
+            getPage(image,page,variant);
+            check_page_segmentation(image);
+            make_page_segmentation_black(image);
+        }
+        void putPageSegmentation(intarray &image,int page,const char *variant=0) {
+            strg v = "pseg";
+            if(variant) { v += "."; v += variant; }
+            check_page_segmentation(image);
+            make_page_segmentation_white(image);
+            putPage(image,page,variant);
+            make_page_segmentation_black(image);
+        }
+
+        void getLineSegmentation(intarray &image,int page,int line,const char *variant=0) {
+            strg v = "rseg";
+            if(variant) { v += "."; v += variant; }
+            getLine(image,page,line,variant);
+            make_line_segmentation_black(image);
+        }
+        void putLineSegmentation(intarray &image,int page,int line,const char *variant=0) {
+            strg v = "rseg";
+            if(variant) { v += "."; v += variant; }
+            make_line_segmentation_white(image);
+            putLine(image,page,line,variant);
+            make_line_segmentation_black(image);
+        }
+
+        void getCharSegmentation(intarray &image,int page,int line,const char *variant=0) {
+            strg v = "cseg";
+            if(variant) { v += "."; v += variant; }
+            getLine(image,page,line,variant);
+            make_line_segmentation_black(image);
+        }
+        void putCharSegmentation(intarray &image,int page,int line,const char *variant=0) {
+            strg v = "cseg";
+            if(variant) { v += "."; v += variant; }
+            make_line_segmentation_white(image);
+            putLine(image,page,line,variant);
+            make_line_segmentation_black(image);
+        }
+
+        void getLattice(IGenericFst &fst,int page,int line,const char *variant=0) {
+            strg s(path(page,line,variant,"fst"));
+            fst.load(s);
+        }
+        void putLattice(IGenericFst &fst,int page,int line,const char *variant=0) {
+            strg s(path(page,line,variant,"fst"));
+            fst.save(s);
+        }
+
     };
 
     IBookStore *make_OldBookStore();
