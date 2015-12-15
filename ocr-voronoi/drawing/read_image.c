48531e583802a59d4b9408646222a57a220a11e5
michaelyin <gt8442b@yahoo.com>
2015-12-15 12:05:38 -0500

    libtiff upgrade.

--- ocr-voronoi/drawing/read_image.c
+++ ocr-voronoi/drawing/read_image.c
@@ -101,7 +101,7 @@ int tiff2imgd(char* fname, ImageData *imgd)
 */
 int tiff_p(char* fname)
 {
-  TIFFHeader hdr;
+  TIFFHeaderClassic hdr;
   int fd;
 
   if((fd=open(fname, O_RDONLY|O_BINARY,0))<0){
@@ -118,7 +118,7 @@ int tiff_p(char* fname)
      hdr.tiff_magic != TIFF_LITTLEENDIAN){
     return(NO);
   }
-  if(hdr.tiff_version != TIFF_VERSION &&
+  if(hdr.tiff_version != TIFF_VERSION_CLASSIC &&
      hdr.tiff_version != II_TIFF_VER){
     return(NO);
   }
--- ocr-voronoi/read_image.cc
+++ ocr-voronoi/read_image.cc
@@ -102,7 +102,7 @@ namespace voronoi{
     */
     int tiff_p(char* fname)
     {
-        TIFFHeader hdr;
+        TIFFHeaderClassic hdr;
         int fd;
 
         if((fd=open(fname, O_RDONLY|O_BINARY,0))<0){
@@ -119,7 +119,7 @@ namespace voronoi{
            hdr.tiff_magic != TIFF_LITTLEENDIAN){
             return(NO);
         }
-        if(hdr.tiff_version != TIFF_VERSION &&
+        if(hdr.tiff_version != TIFF_VERSION_CLASSIC &&
            hdr.tiff_version != II_TIFF_VER){
             return(NO);
         }
